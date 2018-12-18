dfa40dfea68a68446f7d1b03447505e65afee9b1
jeanlego <jeanphilippe.legault@unb.ca>
2018-12-18 19:26:36 -0400

    include flags for travis

--- build_on_travis.sh
+++ build_on_travis.sh
@@ -8,13 +8,13 @@ cd ${SCRIPT_ROOT_DIR}
 ######
 # build libarchive (bsdtar)
 mkdir -p libarchive
-curl -s ${LIBARCHIVE} | tar xvf - -C libarchive/
+curl -s ${LIBARCHIVE} | tar xvzf - -C libarchive/
 cd libarchive
 ./build/autogen.sh
 ./configure
 make -j2
 make install
-export PATH=${PATH}:${PWD}
+export PATH=${PATH}:${SCRIPT_ROOT_DIR}/libarchive
 
 cd ${SCRIPT_ROOT_DIR}
 #######
