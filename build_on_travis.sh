26e381d7fa6e7115961e6b32a0369e6bc3276f28
jeanlego <jeanphilippe.legault@unb.ca>
2018-12-18 20:05:27 -0400

    fix bsdtar

--- build_on_travis.sh
+++ build_on_travis.sh
@@ -1,20 +1,19 @@
 #/bin/bash
 LIBARCHIVE_V=3.3.3
-LIBARCHIVE=https://www.libarchive.org/downloads/libarchive-${LIBARCHIVE_V}.tar.gz
-
+LIBARCHIVE_TARGZ=libarchive-${LIBARCHIVE_V}.tar.gz
+LIBARCHIVE=https://www.libarchive.org/downloads/${LIBARCHIVE_TARGZ}
 SCRIPT_ROOT_DIR=${PWD}
 
 cd ${SCRIPT_ROOT_DIR}
 ######
 # build libarchive (bsdtar)
-mkdir -p libarchive
-curl -s ${LIBARCHIVE} | tar xvzf - -C libarchive/
-cd libarchive
-./build/autogen.sh
+curl -s ${LIBARCHIVE} | tar xvzf
+cd ${LIBARCHIVE_TARGZ}
+build/autogen.sh
 ./configure
 make -j2
 make install
-export PATH=${PATH}:${SCRIPT_ROOT_DIR}/libarchive
+export PATH=${PATH}:${SCRIPT_ROOT_DIR}/${LIBARCHIVE_TARGZ}
 
 cd ${SCRIPT_ROOT_DIR}
 #######
