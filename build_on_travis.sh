8ac467b859b5024b0d66cfdffa7f46a4a05866b5
jeanlego <jeanphilippe.legault@unb.ca>
2018-12-18 20:22:47 -0400

    final commit for bsdtar

--- build_on_travis.sh
+++ build_on_travis.sh
@@ -1,19 +1,21 @@
 #/bin/bash
+set -e
+
 LIBARCHIVE_V=3.3.3
-LIBARCHIVE_TARGZ=libarchive-${LIBARCHIVE_V}.tar.gz
+LIBARCHIVE_FILE=libarchive-${LIBARCHIVE_V}
+LIBARCHIVE_TARGZ=${LIBARCHIVE_FILE}.tar.gz
 LIBARCHIVE=https://www.libarchive.org/downloads/${LIBARCHIVE_TARGZ}
 SCRIPT_ROOT_DIR=${PWD}
 
 cd ${SCRIPT_ROOT_DIR}
 ######
 # build libarchive (bsdtar)
-curl -s ${LIBARCHIVE} | tar xvzf
-cd ${LIBARCHIVE_TARGZ}
+curl -s ${LIBARCHIVE} | tar xz
+cd ${LIBARCHIVE_FILE}
 build/autogen.sh
 ./configure
 make -j2
-make install
-export PATH=${PATH}:${SCRIPT_ROOT_DIR}/${LIBARCHIVE_TARGZ}
+export PATH=${PATH}:${SCRIPT_ROOT_DIR}/${LIBARCHIVE_FILE}
 
 cd ${SCRIPT_ROOT_DIR}
 #######
