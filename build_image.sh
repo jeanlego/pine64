863a591eaa7280cde2f634142dd3943967cb919f
jeanlego <jeanphilippe.legault@unb.ca>
2019-04-30 19:30:17 -0300

    fixup

--- build_image.sh
+++ build_image.sh
@@ -4,7 +4,8 @@ set -e
 
 ROOT_DIR=${PWD}
 export LC_ALL=C
-TEMP_ROOT="build"
+TEMP_ROOT=$(mktemp -d)
+IMAGE_NAME="aarch64_headless.img"
 
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
@@ -12,50 +13,43 @@ if [ "$(id -u)" != "0" ]; then
     exec sudo "$0" "$@" 
 fi
 
-IMAGE_NAME=$1
-if [ "_${IMAGE_NAME}" == "_" ]
+IMAGE_SIZE=$1
+if [ "_${IMAGE_SIZE}" == "_" ]
 then
-    echo "please specify an image name"
+    echo "please specify a size for the image in MB"
     exit 1
 fi
 
-IMAGE_SIZE=$2
-if [ "_${IMAGE_SIZE}" == "_" ]
-then
-    echo "please specify a size for the image ie. 3G or 300M"
-    exit 1
-fi
+trap cleanup EXIT
+
+cleanup() {
+    echo "unmounting the fakeroot"
+    sync
+    umount ${TEMP_ROOT}     2>&1 > /dev/null || /bin/true
+    rm -Rf ${TEMP_ROOT}     2>&1 > /dev/null || /bin/true
+    losetup -d $LOOP_DEVICE 2>&1 > /dev/null || /bin/true
+}
 
 ###################
 # build the empty image
 truncate -s $IMAGE_SIZE $IMAGE_NAME
 
-# zero the beginning of the image
-dd if=/dev/zero of=$IMAGE_NAME bs=1M count=32
-
-echo "Creating filesystems"
-(
-echo o # Create a new empty DOS partition table
-echo n # Add a new partition
-echo p # Primary partition
-echo 1 # Partition number
-echo 32768
-echo   # Last sector (Accept default: varies)
-echo p # print layout
-echo w # Write changes
-) | sudo fdisk $IMAGE_NAME
+parted --script ${IMAGE_NAME} mklabel msdos
 
 echo "Setting up idbloader"
-wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/idbloader.img
+[ ! -f idbloader.img ] && wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/idbloader.img
 dd if=idbloader.img of=$IMAGE_NAME seek=64 conv=notrunc
 
 echo "Setting up uboot"
-wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/uboot.img
+[ ! -f uboot.img ] && wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/uboot.img
 dd if=uboot.img of=$IMAGE_NAME seek=16384 conv=notrunc
 
 echo "Setting up trust"
-wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/trust.img
+[ ! -f trust.img ] && wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/trust.img
 dd if=trust.img of=$IMAGE_NAME seek=24576 conv=notrunc
+
+parted --script ${IMAGE_NAME} mkpart primary ext4 32768s 100%
+parted --script ${IMAGE_NAME} "set" 1 boot on
 
 echo "Attaching loop device"
 LOOP_DEVICE=$(losetup -f)
@@ -65,40 +59,16 @@ mkfs.ext4 -L root ${LOOP_DEVICE}p1
 
 echo "Mounting rootfs"
 mkdir -p ${TEMP_ROOT}
-mount ${LOOP_DEVICE}p1 $TEMP_ROOT
+mount ${LOOP_DEVICE}p1 $TEMP_ROOT 
+sync
 
 echo "mounted the image on ${LOOP_DEVICE}p1->$TEMP_ROOT"
 
-sync
+echo "Downloading rootfs tarball ..."
+[ ! -f ArchLinuxARM-aarch64-latest.tar.gz ] && wget http://archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
 
-trap cleanup EXIT
-
-cleanup() {
-    losetup -d $LOOP_DEVICE
-    umount ${TEMP_ROOT}
-    rm -Rf ${TEMP_ROOT}
-}
-
-QEMU_ARCHES=$3
-if [ "_${QEMU_ARCHES}" == "_" ]
-then
-    echo "please specify an architecture"
-    exit 1
-fi
-
-ROOTFS_URL=$4
-if [ "_${ROOTFS_URL}" == "_" ]
-then
-    echo "please specify an URL for the rootfs"
-    exit 1
-else
-    echo "Downloading rootfs tarball ..."
-    wget $ROOTFS_URL || (echo "cannot download image" && exit 1)
-    # Extract with BSD tar
-    echo -n "Extracting ... "
-    bsdtar -xpf "$(basename ${ROOTFS_URL})" -C "${TEMP_ROOT}"
-    echo "OK"
-fi
+echo "Extracting rootfs tarball ..."
+bsdtar -xpf ${ROOT_DIR}/ArchLinuxARM-aarch64-latest.tar.gz -C "${TEMP_ROOT}"
 
 echo "placing boot.scr @ /boot"
 wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/boot.scr -O ${TEMP_ROOT}/boot/boot.scr
@@ -106,20 +76,12 @@ wget http://os.archlinuxarm.org/os/rockchip/boot/rock64/boot.scr -O ${TEMP_ROOT}
 echo "Mounting system partitions for chrooting"
 mv ${TEMP_ROOT}/etc/resolv.conf ${TEMP_ROOT}/etc/resolv.conf.bckup
 cp /etc/resolv.conf ${TEMP_ROOT}/etc/resolv.conf
-cp $(which qemu-${QEMU_ARCHES}-static) ${TEMP_ROOT}/usr/bin/qemu-${QEMU_ARCHES}-static
-
+cp $(which qemu-aarch64-static) ${TEMP_ROOT}/usr/bin/qemu-aarch64-static
 cp second_phase.sh ${TEMP_ROOT}/opt/second_phase.sh
-systemd-nspawn -D ${TEMP_ROOT} "/opt/second_phase.sh"
-rm -f ${TEMP_ROOT}/opt/second_phase.sh
 
+systemd-nspawn -D ${TEMP_ROOT} "/opt/second_phase.sh"
+
+rm -f ${TEMP_ROOT}/opt/second_phase.sh
 rm -f ${TEMP_ROOT}/usr/bin/qemu-*
 rm -f ${TEMP_ROOT}/etc/resolv.conf
 mv ${TEMP_ROOT}/etc/resolv.conf.bckup ${TEMP_ROOT}/etc/resolv.conf 
-
-umount ${TEMP_ROOT}
-losetup -d $LOOP_DEVICE
-rm -Rf ${TEMP_ROOT}
-
-
-
-
